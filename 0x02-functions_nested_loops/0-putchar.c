f365a2290189b9ff640018ebf69e3f3aa987000e
devAsiyah <aaasiyah07@gmail.com>
2023-02-22 13:45:21 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
-#include <unistd.h>
-#include "_putchar.h"
 #include "main.h"
+#include "_putchar.h"
 
 /**
  * main - Check description
@@ -12,7 +11,7 @@ int main(void)
 	char word[8] = "_putchar";
 	int i;
 
-	for (i = 0; i < i8; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
