f25fccab0339b721fd39330813d12ac98787ca34
devAsiyah <aaasiyah07@gmail.com>
2023-03-02 15:51:15 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,27 @@
-#include "mai.h"
+#include "main.h"
 
 /**
  * rot13 -  a function that encodes a string using rot13.
  * @s: An input string to encode using rot13
  * Return: An encode string
  */
-char *rot13(const char *src)
+char *rot13(char *s)
 {
-    if(src == NULL){
-      return NULL;
-    }
-  
-    char* result = malloc(strlen(src));
-    
-    if(result != NULL){
-      strcpy(result, src);
-      char* current_char = result;
-      
-      while(*current_char != '\0'){
-        //Only increment alphabet characters
-        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
-          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
-            //Characters that wrap around to the start of the alphabet
-            *current_char -= 13;
-          }else{
-            //Characters that can be safely incremented
-            *current_char += 13;
-          }
-        }
-        current_char++;
-      }
-    }
-    return result;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (s);
 }
